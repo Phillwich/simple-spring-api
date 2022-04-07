c56a5d5209271ccf91f9b2e594861828c40db173
Philipp <philipphellwich@MacBook-Pro-von-Philipp.local>
2022-04-07 17:15:14 +0200

    :tada:

--- src/main/java/com/Bootcamp/BootcampApplication.java
+++ src/main/java/com/Bootcamp/BootcampApplication.java
@@ -2,11 +2,20 @@ package com.Bootcamp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
 
 @SpringBootApplication
 public class BootcampApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BootcampApplication.class, args);
+	}
+
+	@Bean
+	public TimedAspect timedAspect(MeterRegistry registry) {
+    	return new TimedAspect(registry);
 	}
 
 }
--- src/main/java/com/Bootcamp/book/BooksController.java
+++ src/main/java/com/Bootcamp/book/BooksController.java
@@ -8,13 +8,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+
 @RestController
 public class BooksController {
 
     private final BooksService booksService;
+    private final Counter bookByYearCounter;
+    private final Counter allBooksCounter;
+    private final Counter bookByNameCounter;
 
-    public BooksController(BooksService booksService) {
+    public BooksController(BooksService booksService, MeterRegistry meterRegistry) {
         this.booksService = booksService;
+        bookByYearCounter = meterRegistry.counter("booksByYearCounter");
+        bookByNameCounter = meterRegistry.counter("bookByNameCounter");
+        allBooksCounter = meterRegistry.counter("allBooksCounter");
     }
 
     @GetMapping("/")
@@ -24,18 +34,19 @@ public class BooksController {
 
     @GetMapping("/get/book/all")
     public List<Books> getBooks() {
+        allBooksCounter.increment();
         return booksService.getBooks();
     }
     
-    
     @GetMapping("/get/book/{name}")
     public Books getBookByName(@PathVariable String name) {
+        bookByNameCounter.increment();
         return booksService.getBookByName(name);
     }
     
-    
     @GetMapping("/get/book/year/{year}")
     public List<Books> getBookByYear(@PathVariable String year) {       
+        bookByYearCounter.increment();
         return booksService.getBookByYear(year);
     }
 }
